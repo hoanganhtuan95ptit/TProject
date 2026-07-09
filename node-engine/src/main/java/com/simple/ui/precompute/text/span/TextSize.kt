feebd53c217e819720d914c946e2b4e513e567fa
Hoang Anh Tuan <hoanganhtuan95ptit@gmail.com>
2026-07-09 10:19:52 +0700

    split service in home

--- node-engine/src/main/java/com/simple/ui/precompute/text/span/TextSize.kt
+++ node-engine/src/main/java/com/simple/ui/precompute/text/span/TextSize.kt
@@ -1,17 +1,19 @@
 package com.simple.ui.precompute.text.span
 
-import com.simple.ui.precompute.text.BigImageSpan
-import com.simple.ui.precompute.text.BigImageSpanConvert
-
 import android.text.style.AbsoluteSizeSpan
 import android.text.style.CharacterStyle
 import androidx.annotation.Keep
 import com.google.auto.service.AutoService
+import com.simple.ui.precompute.text.BigImageSpan
+import com.simple.ui.precompute.text.BigImageSpanConvert
 
 /**
  * Span đặt kích thước chữ tuyệt đối theo đơn vị dp (device-independent pixels).
  * Dùng để kiểm soát font size của title toolbar, header... từ ViewModel.
  */
+
+fun BigTextSize(sizePx: Float) = BigTextSize(sizePx = sizePx.toInt())
+
 data class BigTextSize(val sizePx: Int) : BigImageSpan
 
 @Keep
